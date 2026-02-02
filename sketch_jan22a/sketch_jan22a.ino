fb60edc27d40c800af87b07c69610ec55c1514cd
MaverickSBray <msb00021@mix.wvu.edu>
2026-02-02 14:00:41 -0500

    Create sketch_jan22a.ino
    basic code for Mag.

---
+++ sketch_jan22a/sketch_jan22a.ino
@@ -0,0 +1,90 @@
+/*
+Small Sat Group 4
+Authors: Maverick Bray
+*/
+
+//value of pins are placeholders
+// Air Core Pins
+const int airCorePin1=1;
+const int airCorePin2=2;
+//Solid Core 1 Pins
+const int solidCore1Pin1=3;
+const int solidCore1Pin2=4;
+//Solid Core 2 Pins
+const int solidCore2Pin1=5;
+const int solidCore2Pin2=6;
+
+void setup() {
+pinMode(airCorePin1, OUTPUT);  
+pinMode(airCorePin2, OUTPUT);
+pinMode(solidCore1Pin1, OUTPUT);
+pinMode(solidCore1Pin2, OUTPUT);
+pinMode(solidCore2Pin1, OUTPUT);
+pinMode(solidCore2Pin2, OUTPUT);  
+Serial.begin(115200);
+}
+//pitch refers to the x-axis. The way you want the direction to go (ex. forward tilt or backwords tilt) is decide by the int paramater 
+void pitch(int direction, int time){
+
+  if(direction !=1 && direction !=2){
+    return; 
+  }
+
+  if(direction == 1){
+    digitalWrite(airCorePin1,HIGH);
+    delay(time);
+    digitalWrite(airCorePin1, LOW);
+  }
+  if(direction == 0){
+    digitalWrite(airCorePin2,HIGH);
+    delay(time);
+    digitalWrite(airCorePin2, LOW);
+  }
+
+}
+// yaw refers to the y-axis
+void yaw(int direction, int time) {
+
+  if(direction !=1 && direction !=2){
+    return; 
+  }
+
+  if(direction == 1){
+    digitalWrite(solidCore1Pin1,HIGH);
+    delay(time);
+    digitalWrite(solidCore1Pin1, LOW);
+  }
+  if(direction == 0){
+    digitalWrite(solidCore1Pin2,HIGH);
+    delay(time);
+    digitalWrite(solidCore1Pin2, LOW);
+  }
+
+}
+// roll refer to the z-axis
+void roll(int direction,int time){
+
+  if(direction !=1 && direction !=2){
+    return; 
+  }
+  
+  if(direction == 1){
+    digitalWrite(solidCore2Pin1,HIGH);
+    delay(time);
+    digitalWrite(solidCore2Pin1, LOW);
+  }
+  if(direction == 0){
+    digitalWrite(solidCore2Pin2,HIGH);
+    delay(time);
+    digitalWrite(solidCore2Pin2, LOW);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //below is test code
+pitch(1,1000);
+yaw(1,1000);
+roll(1,1000);
+}
